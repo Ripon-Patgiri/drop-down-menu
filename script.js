3914d5e47f0fad5b3402f33461b3c73a70da8167
Ripon-Patgiri <riponmongzeell@gmail.com>
2022-12-28 20:31:55 +0530

    completed functioning of drop down menu.

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-const dropdownTriggers = document.querySelectorAll(".dropdown-trigger");
+document.addEventListener("click", (e) => {
+  const isDropdownButton = e.target.matches("[data-dropdown-button]");
+  if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return;
 
-dropdownTriggers.forEach((trigger) => {
-  trigger.addEventListener("click", (e) => {
-    e.preventDefault();
-    const dropdown = trigger.nextElementSibling;
-    dropdown.classList.toggle("visible");
+  let currentDropDown;
+  if (isDropdownButton) {
+    currentDropDown = e.target.closest("[data-dropdown]");
+    if (currentDropDown) {
+      currentDropDown.classList.toggle("active");
+    }
+  }
+
+  document.querySelectorAll("[data-dropdown].active").forEach((dropdown) => {
+    if (dropdown && dropdown !== currentDropDown) {
+      dropdown.classList.remove("active");
+    }
   });
 });
